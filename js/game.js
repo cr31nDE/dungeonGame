103eb4a810ac9b05ef83407512afcdb27d7de42a
Jonas Gersting <jonasgersting@web.de>
2023-10-17 01:36:09 +0200

    change

--- js/game.js
+++ js/game.js
@@ -9,7 +9,8 @@ let character = new Image();
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    character.src = '../img/charakters/Fire vizard/Walk.png'
+    character.src = 'img/charakters/Fire vizard/Walk.png'
+    
     
 
     
